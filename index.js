6ec73fff47d2b99b2133fda20dac8330506af738
pavani <pavaniakkirala@gmail.com>
2025-07-31 16:02:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('welcome to DevOps World\n');
+  res.end('welcome to DevOps World \n This is my first POC \n');
 });
 
 server.listen(port, () => {
